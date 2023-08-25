000675a73207bfa4a3cf5ed7d1dc24d74710edd6
chirag <chirag@raapid.ai>
2023-08-25 18:56:09 +0530

    change

--- demo_extension/content.js
+++ demo_extension/content.js
@@ -1,8 +1,37 @@
-(function() {
-  const originalFetch = window.fetch;
-  window.fetch = function(input, init) {
-    console.log("Intercepted fetch:", input);
-    return originalFetch.apply(this, arguments);
-  };
-})();
+// content.js
+function f1() {
+            console.log("t0 ");
+            scanElement(document.querySelectorAll("body > *"));
+        }
+
+        function scanElement(elements) {
+            console.log("t1 ");
+            if(elements != null) {
+                debugger;
+                console.log("t2  : "+elements);
+                elements.forEach(element => {
+                    console.log("t3 ");
+                    handleClick(element);
+                    scanElement(element.childNodes);
+                });
+            }
+        }
+
+        function handleClick(element) {
+            console.log("t4 : "+element.style.cursor+", : "+element.nodeName);
+            if(element.style.cursor == 'pointer' || element.nodeName == 'BUTTON') {
+                    element.click();
+                }
+        }
+
+function changeDivBackground() {
+  var divElements = document.querySelectorAll('div');
+  for (var i = 0; i < divElements.length; i++) {
+    divElements[i].style.backgroundColor = 'blue'; // Change this to the desired color
+  }
+}
+
+// Wait for the DOM to load and then apply changes
+document.addEventListener('DOMContentLoaded', f1);
+
 
