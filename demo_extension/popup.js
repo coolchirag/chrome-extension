711f0de0f6ee611d3a47577a652e8c474538acf4
chirag <chirag@raapid.ai>
2023-08-28 12:31:12 +0530

    make popup working

--- demo_extension/popup.js
+++ demo_extension/popup.js
@@ -1,7 +1,10 @@
 debugger;
 const currentDate = new Date();
 console.log("popup loaded : "+currentDate.getTime());
+document.addEventListener('DOMContentLoaded', function() {
+	var captureButton = document.getElementById('pb1');
+	captureButton.addEventListener('click', popupButtonClicked);
+  });
 function popupButtonClicked() {
 	console.log("pop up clicked");
 }
-//document.getElementById("pb1").addEventListener("click",popupButtonClicked);
